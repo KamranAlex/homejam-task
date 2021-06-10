408df9740b3a66b15aae16c6bb3662f9139815b1
KamranAlex <kamrana610@gmail.com>
2021-06-10 22:22:46 +0600

    circle done

--- src/components/Header/HeroSection/HeroSection.js
+++ src/components/Header/HeroSection/HeroSection.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import './HeroSection.css';
+import './circle.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import Calender from '../../../images/calender.png';
 
 const HeroSection = () => {
   return (
@@ -18,6 +22,56 @@ const HeroSection = () => {
                   <br></br>
                   artists, and never miss out.
                 </p>
+              </div>
+            </div>
+          </div>
+
+          <div className='row justify-content-center'>
+            <div className='col-md-3 circle'>
+              <div className='circle1'>
+                <div className='text-center'>
+                  <div className='content1'>
+                    <FontAwesomeIcon
+                      icon={faHeart}
+                      style={{ color: '#FFF', fontSize: '22px' }}
+                    />
+                    <p>0</p>
+                    <span>Label</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-3 circle'>
+              <div className='circle2'>
+                <div className='text-center'>
+                  <div className='content2'>
+                    <img src={Calender} alt='' />
+                    <p>0</p>
+                    <span>Label</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-3 circle'>
+              <div className='circle3'>
+                <div className='text-center'>
+                  <div className='content3'>
+                    <img src={Calender} alt='' />
+                    <p>0</p>
+                    <span>Label</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-3 circle'>
+              <div className='circle4'>
+                <div className='text-center'>
+                  <div className='content4'>
+                    <img src={Calender} alt='' />
+                    <p>0</p>
+                    <span>Label</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
